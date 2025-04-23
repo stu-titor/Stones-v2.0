78e6ae971ddb144d36c3f1628b3742cb3ba7c597
Stu <84202775+Sturext@users.noreply.github.com>
2025-04-23 12:30:27 -0500

    Update StonesClient.java

--- src/client/java/stu/stones/client/StonesClient.java
+++ src/client/java/stu/stones/client/StonesClient.java
@@ -6,6 +6,7 @@ public class StonesClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-
+        ModModelLayers.register();
+        EntityRendererRegistry.register(ModEntities.CUSTOM_ZOMBIE, CustomZombieRenderer::new);
     }
 }
